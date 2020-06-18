e6b1dc3c8f7696c67081caab937f0f0094a536e8
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-18 00:27:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,17 @@
 #write your code here
 
-def countdown
-  n = gets.chomp
-  while n >= 0
-   puts "#{n} SECOND(S)!"
-   n -= 1 
- end
-returns "HAPPY NEW YEAR!"  
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number}" SECOND(S)!
+    sleep(1)
+    number -= 1
+  end
 end
